711b58e83e89a4872091c84122ef04e3e1abbfe6
Sopheak Teng <sopheakteng112@gmail.com>
2026-09-01 00:29:16 -0500

    feat: variables and data types

---
+++ main.js
@@ -0,0 +1,8 @@
+// 1. Variables and Data Types
+let personName = "Sopheak Teng";
+var age = 21;
+const isStudent = true;
+
+console.log("Name:", personName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
