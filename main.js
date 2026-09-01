4309cda6e961a425f06f789499b3c3aae8bc5bb1
Sopheak Teng <sopheakteng112@gmail.com>
2026-09-01 00:31:21 -0500

    feat: add basic arithmetic operations

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ const isStudent = true;
 console.log("Name:", personName);
 console.log("Age:", age);
 console.log("Is Student:", isStudent);
+
+// 2. Basic Arithmetic Operations
+let num1 = 10;
+let num2 = 5;
+
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiplication = num1 * num2;
+var division = num1 / num2;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
